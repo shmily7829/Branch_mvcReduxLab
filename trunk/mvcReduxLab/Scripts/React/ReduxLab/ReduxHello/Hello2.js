b64d754337dc1e5871379b1c904b710ff96b7ccb
SherryLin <shmily7829@gmail.com>
2020-03-23 18:38:17 +0800

    add moment datetime

--- trunk/mvcReduxLab/Scripts/React/ReduxLab/ReduxHello/Hello2.js
+++ trunk/mvcReduxLab/Scripts/React/ReduxLab/ReduxHello/Hello2.js
@@ -1,12 +1,15 @@
 ﻿import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import moment from 'moment'
 
 /// Stateless Component --- 一般用於單向展示資料
 const Hello2 = (props) => (
     <div className="container">
         {props.hello}
+        <h2>{moment().format('YYYY-MM-DDTHH:mm:ssZ')}</h2>
     </div>
 )
+
 
 // connect to Store
 const mapStateToProps = (state, ownProps) => ({
